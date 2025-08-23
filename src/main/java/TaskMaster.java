cb764c6882c31e162ed4f5684bd48fc0c61f1d12
Emannuel Tan Jing Yue <emannueltanjingyue@gmail.com>
2025-08-23 18:15:06 +0800

    TaskMaster.java: Change Start and Ending Messages to use constants

--- src/main/java/TaskMaster.java
+++ src/main/java/TaskMaster.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class TaskMaster {
     public static void main(String[] args) {
+        // Create Constants for future use
         String spacing = "⎯";
         spacing = spacing.repeat(70) + "\n";
-        String logo = " _____   ___   ____  |  /   |\\  /|  ___   ____  _____   ____  ___   \n"
-                +     "|__ __| |   | |      | /    | \\/ | |   | |     |__ __| |     |   \\ \n"
-                +     "  | |   |___| |___   |      |    | |___| |___    | |   |____ |___/   \n"
-                +     "  | |   |   |     |  | \\    |    | |   |     |   | |   |     |   \\ \n"
-                +     "  |_|   |   | ____|  |  \\   |    | |   | ____|   |_|   |____ |    \\\n";
+        final String LOGO = " _____   ___   ____  |  /   |\\  /|  ___   ____  _____   ____  ___   \n"
+                +           "|__ __| |   | |      | /    | \\/ | |   | |     |__ __| |     |   \\ \n"
+                +           "  | |   |___| |___   |      |    | |___| |___    | |   |____ |___/   \n"
+                +           "  | |   |   |     |  | \\    |    | |   |     |   | |   |     |   \\ \n"
+                +           "  |_|   |   | ____|  |  \\   |    | |   | ____|   |_|   |____ |    \\\n";
+        final String START_MESSAGE = spacing + "Hello I'm\n" + LOGO + "\nWhat can I do for you?\n" + spacing;
+        final String END_MESSAGE = spacing + "Bye. Hope to see you again soon!\n" + spacing;
 
-        System.out.println(spacing + "Hello I'm\n" + logo + "\nWhat can I do for you?\n");
-        System.out.println(spacing + "Bye. Hope to see you again soon!\n");
-        System.out.print(spacing);
+        // Opening message output
+        System.out.print(START_MESSAGE);
+
+        // Ending message output
+        System.out.print(END_MESSAGE);
+
     }
 }
