3d8b61ac7b0d1f0f17c82eb658f91bf84b6c9401
Emannuel Tan Jing Yue <emannueltanjingyue@gmail.com>
2025-08-30 19:20:12 +0800

    Add class Event.java that inherits from class ToDo

---
+++ src/main/java/Event.java
@@ -0,0 +1,17 @@
+public class Event extends ToDo {
+    protected String from;
+    protected String to;
+
+    // Constructor
+    public Event(String desciption, String from, String to) {
+        super(desciption);
+        type = TaskType.E;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String getStatus() {
+        return super.getStatus() + "(from: " + from + " to: " + to + ")";
+    }
+}
