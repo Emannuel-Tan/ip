0352da84cd2bd5e9504d1fdec7eeb56f3d477e5c
Emannuel Tan Jing Yue <emannueltanjingyue@gmail.com>
2025-09-20 16:06:51 +0800

    TaskMaster.java: Refactor code

--- src/main/java/taskmaster/TaskMaster.java
+++ src/main/java/taskmaster/TaskMaster.java
@@ -9,33 +9,25 @@ public class TaskMaster {
 
     // Main Method
     public static void main(String[] args) {
-        // Opening message output
         Ui ui = new Ui();
         ui.startMessage();
 
-        // File Operation
         Storage storage = new Storage("./data/TaskMaster.txt");
 
-        // Create Task ArrayList using data from file
         TaskList taskList = new TaskList(storage.readFile(), ui);
         Parser parser = new Parser(taskList, ui);
 
-        // Take User Input
         String userInput = ui.getNextLine();
 
         // Main Loop (loop until "bye" command given)
         while (!userInput.startsWith("bye")) {
-            // Try a command with exception handling
-            parser.tryCommand(userInput);
+            parser.tryCommand(userInput); // Try a command with exception handling
 
-            // Get next input
             userInput = ui.getNextLine();
         }
 
-        // Ending message output
-        ui.endMessage();
-
-        // Output data to file
         storage.writeToFile(taskList.getTasks());
+
+        ui.endMessage();
     }
 }
